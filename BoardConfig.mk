9dd693e7d7ca0d976af0e3738c204f4c3af1e0a0
Michael Bestas <mkbestas@gmail.com>
2022-02-21 22:31:16 +0200

    Inherit vendor/calyx FCM
    Change-Id: I78ea2cf4e88dd759ad80f3f54b54a2e4b3dbb3aa

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,7 +79,7 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/calyx/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 ODM_MANIFEST_FILES := $(DEVICE_PATH)/manifest-qva.xml
