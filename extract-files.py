e99dbd715d9cf7f6edbcfe70982e4a012632a3e6
Chirayu Desai <chirayudesai1@gmail.com>
2022-10-14 01:07:43 +0530

    proprietary: Revert FP4D vodafone DE APN changes
    * Patch generated against TP2N
    
    Change-Id: Ib7929400693ab872ce91fd73f3385e7f545f54fe

--- extract-files.py
+++ extract-files.py
@@ -43,6 +43,8 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'product/etc/apns-conf.xml': blob_fixup()
+        .patch_file('apns-conf.xml.patch'),
     'vendor/etc/libnfc-hal-st.conf': blob_fixup()
         .regex_replace('STNFC_HAL_LOGLEVEL=.*', 'STNFC_HAL_LOGLEVEL=0x12'),
     'vendor/lib64/hw/fingerprint.lito.so': blob_fixup()
