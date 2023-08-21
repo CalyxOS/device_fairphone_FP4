906e9a113c764375fabad1ee98ecff5354255e54
Michael Bestas <mkbestas@gmail.com>
2023-08-21 14:59:54 +0300

    Bump stock SPL to July 2023, FP4.SP2J.B.086.20230807
    * https://forum.fairphone.com/t/software-update-fp4-sp2j-b-086-20230807/99042
    
    Change-Id: I13f247e731f995edff0056741fa0f18b88953d76

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -202,7 +202,7 @@ VENDOR_SECURITY_PATCH := 2023-06-05
 
 # Calyx
 PLATFORM_SECURITY_PATCH := 2023-06-05
-STOCK_SECURITY_PATCH := 2023-06-05 # Latest stock update available
+STOCK_SECURITY_PATCH := 2023-07-05 # Latest stock update available
 PLATFORM_SECURITY_PATCH_TIMESTAMP := $(shell date -d 'TZ="GMT" $(STOCK_SECURITY_PATCH)' +%s)
 
 # SEPolicy
