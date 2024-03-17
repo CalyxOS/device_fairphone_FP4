0211abe2d739df31f2024429bfefa274a8f35859
Matthias Junker-Petschick <razzle@razzle.de>
2024-03-17 02:17:41 +0100

    FP4: Extract additional files to factory directory
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: I6d89ad55d857c6a60e9d2c28edd320c984f00244

--- extract-files.py
+++ extract-files.py
@@ -70,6 +70,7 @@ module = ExtractUtilsModule(
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
     add_firmware_proprietary_file=True,
+    add_factory_proprietary_file=True,
 )
 
 if __name__ == '__main__':
