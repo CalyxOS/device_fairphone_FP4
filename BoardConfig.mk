5412c6d31ad1d8a2df8ba155dd698e003649e57a
Michael Bestas <mkbestas@gmail.com>
2025-04-02 16:06:56 +0300

    Bump AVB rollback index to latest stock
    Change-Id: Ica02168ada1f9ffaebfd60b6f965bbba0e7f94be

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -184,7 +184,7 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Verified Boot
-STOCK_SECURITY_PATCH := 2025-01-05 # Latest stock update available
+STOCK_SECURITY_PATCH := 2025-03-05 # Latest stock update available
 BOARD_AVB_ENABLE := true
 BOARD_AVB_VBMETA_SYSTEM := system system_ext product
 BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
